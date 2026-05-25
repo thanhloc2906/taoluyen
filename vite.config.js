e78bd025f3b33e60bb9349f0d4bc2b2ed53608ef
thanhloc2906 <162592816+thanhloc2906@users.noreply.github.com>
2026-05-25 21:12:10 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/taoluyen/', // BẮT BUỘC PHẢI CÓ DÒNG NÀY ĐỂ KHÔNG BỊ TRẮNG TRANG
 })
